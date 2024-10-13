2fa1376b906eb3c3312cc829204b44bff565f0e1
Alecsei Uzhva <stas.basov@whatever.com>
2024-10-13 22:47:52 +0300

    fixed settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = get_random_secret_key()
 
-DEBUG = os.getenv('DEBUG', default=False)
+if os.getenv('DEBUG', default='false').lower() == 'true':
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['123.123.123.123', 'localhost', 'addenerinfrasprint1.ddns.net']
 
