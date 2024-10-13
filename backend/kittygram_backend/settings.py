17aa26b30bcc023b101034ff43fea5bf47a130c1
Alecsei <alecsei.uzhva@yandex.ru>
2024-10-14 02:12:17 +0300

    fix settings secret_key1/2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,9 +8,7 @@ from django.core.management.utils import get_random_secret_key
 load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
-if not SECRET_KEY:
-    SECRET_KEY = get_random_secret_key()
+SECRET_KEY = get_random_secret_key()
 
 DEBUG = os.getenv('DEBUG', default=False)
 
