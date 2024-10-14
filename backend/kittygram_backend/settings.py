14fccf28f22128f92251c4a71391e50513fe9062
Alecsei Uzhva <stas.basov@whatever.com>
2024-10-15 00:02:57 +0300

    allowed_hosts rebuild2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,8 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
 DEBUG = os.environ.get('DEBUG', default='false').lower() == 'true'
 
-ALLOWED_HOSTS = [host.strip() for host in
-                 os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
